b06d3eac7794a98489cbf47f4b806f6b2990ff58
Fabien Grasser <fabien.grasser@googlemail.com>
2019-07-03 16:37:28 +0200

    fix errors according to Rule PH1, PH2, PH3 reported by JEDchecker. (#2)
    * fix errors according to Rule PH1, PH2, PH3 reported by JEDchecker.
    Also add an update server tag referencing a new manifest.xml
    where future updates may be defined
    
    * ignore phpstrom config dir
    
    * move manifest.xml and adjust the update example inside
    
    * small change to example update
    
    * uncomment update tag
    
    * bump version

--- src/packages/com_newsletter2go/site/views/widget/tmpl/default.php
+++ src/packages/com_newsletter2go/site/views/widget/tmpl/default.php
@@ -1,4 +1,8 @@
 <?php
+
+/**
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die;
 
 echo '<div id="n2goWidgetPreview" class="moduletable">' . $this->widget . '</div>';
--- src/packages/mod_newsletter2go/helper.php
+++ src/packages/mod_newsletter2go/helper.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 defined('_JEXEC') or die;
 
 class ModNewsletter2GoHelper
